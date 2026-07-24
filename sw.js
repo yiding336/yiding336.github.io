dcdd091f3a2323ed77bf0871fbab86a4b8acb0fd
yiding336 <yuanyiding2026@outlook.com>
2026-07-24 19:12:05 +0800

    Site updated: 2026-07-24 19:12:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1784806291041";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784891517198";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
